337f27f7aaa9f035b7295e9aade04ddcf862c08e
roseline124 <guseod24@gmail.com>
2020-10-07 00:12:22 +0900

    Updates

---
+++ precache-manifest.0d88bbdcdd1ce550ced145f822911925.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4328a018bfa2fe8bda6b1cbf794db8f1",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "91723b0c6b06946faab3",
+    "url": "/nwitter/static/js/2.cf4b5808.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/nwitter/static/js/2.cf4b5808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbcdc63389eba7af6e85",
+    "url": "/nwitter/static/js/main.6a73f267.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
